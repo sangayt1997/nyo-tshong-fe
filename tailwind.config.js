29d4f047af57099ddacc394dfed591f537fa5d2a
Sangay Thinley <thinley.sangay@selise.ch>
2023-07-13 19:21:23 +0600

    refactor(nyo-tshong-fe): adding content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}'],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
     plugins: [require('daisyui')],
 };
