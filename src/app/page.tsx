3f1396caa30b050871c7aabb82626cc797388dcd
Sangay Thinley <thinley.sangay@selise.ch>
2023-07-16 23:53:23 +0600

    fix(nyo-tshong-fe): fix vercel build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
             <button className="btn btn-primary">Hello daisyUI</button>
             <input type="checkbox" className="checkbox checkbox-secondary"/>
             <div className="collapse collapse-arrow bg-base-200">
-                <input type="radio" name="my-accordion-2" checked="checked"/>
+                <input type="radio" name="my-accordion-2" checked={true} />
                 <div className="collapse-title text-xl font-medium">
                     Click to open this one and close others
                 </div>
