d23b8f3364965c87ac8ca3e748e4443e5f2709ce
Sangay Thinley <thinley.sangay@selise.ch>
2023-07-16 23:49:48 +0600

    refactor(nyo-tshong-fe): testing component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
 export default function Home() {
-  return (
-    <main>
-      <p>FakeStore API</p>
-        <button className="btn btn-primary">Hello daisyUI</button>
-        <input type="checkbox" className="checkbox checkbox-secondary"/>
-    </main>
-  )
+    return (
+        <main>
+            <p>FakeStore API</p>
+            <button className="btn btn-primary">Hello daisyUI</button>
+            <input type="checkbox" className="checkbox checkbox-secondary"/>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" checked="checked"/>
+                <div className="collapse-title text-xl font-medium">
+                    Click to open this one and close others
+                </div>
+                <div className="collapse-content">
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2"/>
+                <div className="collapse-title text-xl font-medium">
+                    Click to open this one and close others
+                </div>
+                <div className="collapse-content">
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2"/>
+                <div className="collapse-title text-xl font-medium">
+                    Click to open this one and close others
+                </div>
+                <div className="collapse-content">
+                    <p>hello</p>
+                </div>
+            </div>
+        </main>
+    )
 }
