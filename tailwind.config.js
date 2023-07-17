97d843e8b148b0e91e1d04df514941d3af1da48d
Sangay Thinley <thinley.sangay@selise.ch>
2023-07-17 21:54:03 +0600

    refactor(nyo-tshong): custom theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,22 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/**/*.{js,ts,jsx,tsx,mdx}'
     ],
+    daisyui: {
+        themes: [
+            {
+                customTheme: {
+                    "primary": "#257eea",
+                    "secondary": "#67e8f9",
+                    "accent": "#a3e635",
+                    "neutral": "#1f2937",
+                    "base-100": "#e0e1eb",
+                    "info": "#1b6fe4",
+                    "success": "#199f4c",
+                    "warning": "#f5b770",
+                    "error": "#f44d3e",
+                },
+            },
+        ],
+    },
     plugins: [require('daisyui')],
 };
