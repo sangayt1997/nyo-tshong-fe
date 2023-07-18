e223a8ee490d025c746e681c0c18ca0f9eeda5ad
Sangay Thinley <thinley.sangay@selise.ch>
2023-07-18 21:03:43 +0600

    refactor(nyo-tshong): adding tailwind typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
         <main>
             <p>FakeStore API</p>
             <button className="btn btn-primary">Hello daisyUI</button>
-            <input type="checkbox" className="checkbox checkbox-secondary"/>
             <div className="collapse collapse-arrow bg-base-200">
                 <input type="radio" name="my-accordion-2" checked={true} />
                 <div className="collapse-title text-xl font-medium">
@@ -31,6 +30,17 @@ export default function Home() {
                     <p>hello</p>
                 </div>
             </div>
+
+            <h1>Garlic bread with cheese: What the science tells us</h1>
+            <p>
+                For years parents have espoused the health benefits of eating garlic bread with cheese to their
+                children, with the food earning such an iconic status in our culture that kids will often dress
+                up as warm, cheesy loaf for Halloween.
+            </p>
+            <p>
+                But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
+                springing up around the country.
+            </p>
         </main>
     )
 }
