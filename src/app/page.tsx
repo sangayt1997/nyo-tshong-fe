690e090f5801c54b7e15a72183342c182aec24c6
Sangay Thinley <thinley.sangay@selise.ch>
2023-07-22 23:12:44 +0600

    refactor(nyo-tshong): remove test component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,44 +3,6 @@ export default function Home() {
         <main>
             <p>FakeStore API</p>
             <button className="btn btn-primary">Hello daisyUI</button>
-            <div className="collapse collapse-arrow bg-base-200">
-                <input type="radio" name="my-accordion-2" checked={true} />
-                <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
-                </div>
-                <div className="collapse-content">
-                    <p>hello</p>
-                </div>
-            </div>
-            <div className="collapse collapse-arrow bg-base-200">
-                <input type="radio" name="my-accordion-2"/>
-                <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
-                </div>
-                <div className="collapse-content">
-                    <p>hello</p>
-                </div>
-            </div>
-            <div className="collapse collapse-arrow bg-base-200">
-                <input type="radio" name="my-accordion-2"/>
-                <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
-                </div>
-                <div className="collapse-content">
-                    <p>hello</p>
-                </div>
-            </div>
-
-            <h1>Garlic bread with cheese: What the science tells us</h1>
-            <p>
-                For years parents have espoused the health benefits of eating garlic bread with cheese to their
-                children, with the food earning such an iconic status in our culture that kids will often dress
-                up as warm, cheesy loaf for Halloween.
-            </p>
-            <p>
-                But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-                springing up around the country.
-            </p>
         </main>
     )
 }
