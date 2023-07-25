5fbd5e335dd11fa214e8c67eb8d3ff02d4ac2b8b
Sangay Thinley <thinley.sangay@selise.ch>
2023-07-25 16:16:51 +0600

    clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
         <main>
             <p>FakeStore API</p>
             <button className="btn btn-primary">Hello daisyUI</button>
+            <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
         </main>
     )
 }
