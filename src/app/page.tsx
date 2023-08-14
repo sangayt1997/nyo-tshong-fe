42f3ded1c760ba7c32dceee693733b7cf1a1a934
Sangay Thinley <thinley.sangay@selise.ch>
2023-08-15 02:07:41 +0600

    feat(nyo-tshong-fe): integrate font family using google font and install sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 export default function Home() {
     return (
         <main>
-            <p>FakeStore API</p>
+            <p className="font-poppins_medium">FakeStore API</p>
             <button className="btn btn-primary">Hello daisyUI</button>
-            <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+            <h1 className="text-3xl underline">Hello, Next.js!</h1>
             <Image
                 src="https://res.cloudinary.com/dsim60jne/image/upload/v1684229456/personal%20website%20asset/stellar_minds_banner_msbzlw.png"
                 width={500}
