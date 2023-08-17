3afe4462fca433be8550b10f65d23a6020ce76a9
Sangay Thinley <thinley.sangay@selise.ch>
2023-08-17 21:37:25 +0600

    refactor(nyo-tshong): font optimization and removing tailwindcss at rule warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
     theme: {
         extend: {
           fontFamily: {
-            poppins: ["'Poppins_Regular'"],
-            poppins_medium: ["'Poppins_Medium'"],
-            poppins_bold: ["'Poppins_Bold'"],
+            poppins: ['var(--font-poppins-regular)'],
+            poppins_medium: ['var(--font-poppins-medium)'],
+            poppins_bold: ['var(--font-poppins-bold)'],
           },
         },
     },
