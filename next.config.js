23955690da94240e8a59404441dce4142cea4c1e
Sangay Thinley <thinley.sangay@selise.ch>
2023-08-21 22:02:39 +0600

    refactor(nyo-tshong): code refactor

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 module.exports = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+      },
     images: {
         domains: ["res.cloudinary.com"],
         remotePatterns: [
