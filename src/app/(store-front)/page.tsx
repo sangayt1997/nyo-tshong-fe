dbbb207f91301f2c8b9922f7d04885d13591ba2f
Sangay Thinley <thinley.sangay@selise.ch>
2023-08-29 20:02:22 +0600

    refactor(nyo-tshong): code refactor

--- src/app/(store-front)/page.tsx
+++ src/app/(store-front)/page.tsx
@@ -17,29 +17,18 @@ export default async function Home() {
 
     return (
         <main>
-            <p className="font-poppins_medium">FakeStore API</p>
-            <button className="btn btn-primary">Hello daisyUI</button>
-            <h1 className="text-3xl underline">Hello, Next.js!</h1>
-            <Image
-                src="https://res.cloudinary.com/dsim60jne/image/upload/v1684229456/personal%20website%20asset/stellar_minds_banner_msbzlw.png"
-                width={500}
-                height={600}
-                alt="picture"
-                style={{width: 'auto', height: 'auto'}}
-                priority={true}
-            />
             <div className="product-list">
                 {products.map((item : any, index: number) => (
                     <ProductCard 
-                    key={index} 
-                    id={item?.id}
-                     image={item?.image} 
-                     title={item?.title} 
-                     description={item?.description} 
-                     price={item?.price}
-                     rate={item?.rating?.rate}
-                     count={item?.rating?.count}
-                     />
+                        key={index} 
+                        id={item?.id}
+                        image={item?.image} 
+                        title={item?.title} 
+                        description={item?.description} 
+                        price={item?.price}
+                        rate={item?.rating?.rate}
+                        count={item?.rating?.count}
+                    />
                 ))}
             </div>
         </main>
