08caca2f32a1f9a915093c6b86a5a87b9073dd2c
Sangay Thinley <thinley.sangay@selise.ch>
2023-08-29 18:47:52 +0600

    feat(nyo-tshong): binding products with rest api

--- src/app/(store-front)/page.tsx
+++ src/app/(store-front)/page.tsx
@@ -1,6 +1,20 @@
-import Image from 'next/image'
+import Image from "next/image";
+import ProductCard from "./product-card/product-card";
 
-export default function Home() {
+async function getProducts() {
+    const res = await fetch('https://fakestoreapi.com/products')
+   
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch FakeApi product data')
+    }
+   
+    return res.json()
+  }
+
+export default async function Home() {
+    const products = await getProducts();
+
     return (
         <main>
             <p className="font-poppins_medium">FakeStore API</p>
@@ -14,6 +28,20 @@ export default function Home() {
                 style={{width: 'auto', height: 'auto'}}
                 priority={true}
             />
+            <div className="product-list">
+                {products.map((item : any, index: number) => (
+                    <ProductCard 
+                    key={index} 
+                    id={item?.id}
+                     image={item?.image} 
+                     title={item?.title} 
+                     description={item?.description} 
+                     price={item?.price}
+                     rate={item?.rating?.rate}
+                     count={item?.rating?.count}
+                     />
+                ))}
+            </div>
         </main>
     )
 }
