b68cf98fd63f4994f73d0899cc8993a4cebab584
Sangay Thinley <thinley.sangay@selise.ch>
2023-09-05 18:13:48 +0600

    refactor(nyo-tshong): using Next Link

--- src/app/(store-front)/product-card/product-card.tsx
+++ src/app/(store-front)/product-card/product-card.tsx
@@ -1,5 +1,6 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ProductCardProps {
     id: string;
@@ -13,21 +14,21 @@ interface ProductCardProps {
 
 export default function ProductCard (props : ProductCardProps) {
     return (
-        <a href="#">
+        <Link href="/">
             <div className="bg-white rounded-[8px] shadow p-[4px] hover:shadow-lg transition duration-300 cursor-pointer" id={props.id}>
-            <Image 
-                src={props.image} 
-                alt={props.title} 
-                width={142}
-                height={124}
-                priority={true}
-            />
-            <p className="text-[16px] font-poppins_medium">{props.title}</p>
-            <p className="text-black/[0.60]">{props.description}</p>
-            <p className="text-black/[0.60]">Category: <span className="text-black/[0.87] font-poppins_medium">{props.category}</span></p>
-            <p className="text-black/[0.60]">Price: <span className="text-black/[0.87] font-poppins_medium">${props.price}</span></p>
-            <p className="text-black/[0.60]">Rating: <span className="text-black/[0.87] font-poppins_medium">{props?.rating?.rate} ({props?.rating?.count} reviews)</span></p>
-      </div>
-        </a>
+                <Image 
+                    src={props.image} 
+                    alt={props.title} 
+                    width={142}
+                    height={124}
+                    priority={true}
+                />
+                <p className="text-[16px] font-poppins_medium">{props.title}</p>
+                <p className="text-black/[0.60]">{props.description}</p>
+                <p className="text-black/[0.60]">Category: <span className="text-black/[0.87] font-poppins_medium">{props.category}</span></p>
+                <p className="text-black/[0.60]">Price: <span className="text-black/[0.87] font-poppins_medium">${props.price}</span></p>
+                <p className="text-black/[0.60]">Rating: <span className="text-black/[0.87] font-poppins_medium">{props?.rating?.rate} ({props?.rating?.count} reviews)</span></p>
+            </div>
+        </Link>
     );
 }
