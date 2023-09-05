87a8211b8310359f9d4b64e8cc0bcad82c2fe6b7
Sangay Thinley <thinley.sangay@selise.ch>
2023-09-05 19:06:54 +0600

    refactor(nyo-tshong): grid view of product cards

--- src/app/(store-front)/page.tsx
+++ src/app/(store-front)/page.tsx
@@ -15,9 +15,9 @@ export default async function Home() {
     const products = await getProducts();
 
     return (
-        <main>
-            <div className="p-[16px]">
-                {products.map((item : any, index: number) => (
+        <main className="p-[16px]">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[24px]">
+                {products.map((item: any, index: number) => (
                     <ProductCard
                         key={index}
                         id={item?.id}
--- src/app/(store-front)/product-card/product-card.tsx
+++ src/app/(store-front)/product-card/product-card.tsx
@@ -15,7 +15,7 @@ interface ProductCardProps {
 export default function ProductCard (props : ProductCardProps) {
     return (
         <Link href="/">
-            <div className="bg-white rounded-[8px] shadow p-[4px] hover:shadow-lg transition duration-300 cursor-pointer" id={props.id}>
+            <div className="bg-white rounded-[8px] shadow p-[16px] transition-transform transform hover:scale-101 hover:shadow-lg transition duration-300 cursor-pointer" id={props.id}>
                 <Image 
                     src={props.image} 
                     alt={props.title} 
@@ -24,7 +24,7 @@ export default function ProductCard (props : ProductCardProps) {
                     priority={true}
                 />
                 <p className="text-[16px] font-poppins_medium">{props.title}</p>
-                <p className="text-black/[0.60]">{props.description}</p>
+                <p className="text-black/[0.60] text-description-truncate">{props.description}</p>
                 <p className="text-black/[0.60]">Category: <span className="text-black/[0.87] font-poppins_medium">{props.category}</span></p>
                 <p className="text-black/[0.60]">Price: <span className="text-black/[0.87] font-poppins_medium">${props.price}</span></p>
                 <p className="text-black/[0.60]">Rating: <span className="text-black/[0.87] font-poppins_medium">{props?.rating?.rate} ({props?.rating?.count} reviews)</span></p>
