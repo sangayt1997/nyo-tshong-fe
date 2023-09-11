af5eecc04a17592dd7b447e1356e77b468f66870
Sangay Thinley <thinley.sangay@selise.ch>
2023-09-11 20:18:55 +0600

    refactor(nyo-tshong): layouting ui for storefront

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -2,24 +2,21 @@ import Link from "next/link";
 
 export default function Header () {
     return (
-        // TODO: Why color getting conflict with daisyUI theme
-        <header className="bg-primary-50 p-[16px]">
-            <div className="container mx-auto flex justify-between items-center">
-                <div>
+        <header className="shadow-bottom p-[24px]">
+            <div className="flex justify-between items-center">
                 <Link href="/">
                     Your Logo
                 </Link>
-                </div>
                 <nav className="space-x-4">
-                <Link href="/">
-                    Home
-                </Link>
-                <Link href="/about">
-                    About
-                </Link>
-                <Link href="/contact">
-                    Contact
-                </Link>
+                    <Link href="/">
+                        Home
+                    </Link>
+                    <Link href="/about">
+                        About
+                    </Link>
+                    <Link href="/contact">
+                        Contact
+                    </Link>
                 </nav>
             </div>
         </header>
